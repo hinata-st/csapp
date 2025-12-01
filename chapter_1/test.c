3fb94c8964ccb1ff674aaf76c6f224691aea46c8
Emilia-tan <1981859435@qq.com>
2025-12-01 19:42:53 +0800

    feat(csapp):build chapter_1 test code

--- chapter_1/test.c
+++ chapter_1/test.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <limits.h>
 
-int main() {
-    printf("\nHello, World!\n");
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len)
+{
+    size_t i;
+    for (i = 0; i < len; i++)
+    {
+        printf(" %.2x", start[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int a = INT_MIN;
+    int b = -a;
+
+    int c = 8;
+    int d = -c;
+
+    show_bytes((byte_pointer)&a, sizeof(int));
+    show_bytes((byte_pointer)&b, sizeof(int));
+    show_bytes((byte_pointer)&c, sizeof(int));
+    show_bytes((byte_pointer)&d, sizeof(int));
     return 0;
 }
