41524423005e75d2cab19fcea7287d16924864bd
Emilia-tan <1981859435@qq.com>
2026-04-11 17:14:11 +0800

    feat(homework): 添加多个示例程序和CMake配置文件

---
+++ chapter_8/homework_code/8.20.c
@@ -0,0 +1,17 @@
+/*
+ * 8.20.c
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[], char *env[])
+{
+    if (execve("/bin/ls", argv, env) == -1)
+    {
+        fprintf(stderr, "execve error: %s\n", strerror(errno));
+        exit(1);
+    }
+}
